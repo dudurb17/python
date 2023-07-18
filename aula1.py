4489e25c934d5186f159b41bed83fed76084a307
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-17 21:32:33 -0300

    python 1

---
+++ aula1.py
@@ -0,0 +1,12 @@
+print('Ola mundo')
+print(5+4)
+print('5'+'3')
+print('Ola', 2)
+nome='Eduardo'
+idade=18
+peso=60.0
+print('Meu nome é',nome, 'idade:',idade,'meu peso é:', peso)
+nome=input('Qual é o seu nome')
+idade=input('Qual a sua idade')
+peso=input('Qual seu peso')
+
