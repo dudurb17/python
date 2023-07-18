f4758e7e542e11485afb35e618c32bdd04d216c4
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-18 08:57:39 -0300

    media e sucessor e antecessor

---
+++ desafios/desafio007.py
@@ -0,0 +1,2 @@
+num = int(input('Informe um valor inteiro '))
+print('Valor é {}, seu sucessor é {} e o antecessor {}'.format(num, num+1, num-1))
---
+++ desafios/desafio008.py
@@ -0,0 +1,4 @@
+nt1 = float(input('Informe a primeira nota: '))
+nt2 = float(input('Informe a segunda nota: '))
+print('A primeira nota foi {} e a segunda {} e a media é {}'.format(
+    nt1, nt2, (nt1+nt2)/2))
