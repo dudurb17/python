ab53714e391f2d2e3a037c9251dafb05b510985f
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-18 09:18:20 -0300

    comprando tinta para pintar a parede

---
+++ desafios/desafio012.py
@@ -0,0 +1,4 @@
+altura = float(input('Informe a altura da parede: '))
+largura = float(input('Informe a largura da parede: '))
+tinta = (altura*largura)/2
+print('Voce precisa comprar {} litros de tinta'.format(tinta))
