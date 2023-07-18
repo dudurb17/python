5a072d09da0afda47c0bb524b508ba43d111e746
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-18 08:09:32 -0300

    desafios simples(soma, data, nome)

--- aula1.py
+++ aula1.py
@@ -10,3 +10,5 @@ nome=input('Qual é o seu nome')
 idade=input('Qual a sua idade')
 peso=input('Qual seu peso')
 
+
+
---
+++ desafios/desafio003.py
@@ -0,0 +1,3 @@
+num1 = int(input('Informe o primeiro numero '))
+num2 = int(input('Informe o segundo numero '))
+print('A soma é', num1+num2)
