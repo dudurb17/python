6666587bd7ba4b58279966e8c468480dd0d80a09
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-18 18:54:22 -0300

    sorteando uma ordem de apresentação

--- desafios/desafio020.py
+++ desafios/desafio020.py
@@ -1,4 +1,4 @@
 import math
 angulo = int(input('Informe o angulo'))
-print('O angulo {} informado foi {} O cosseno é O seno é A tangente é '.format(
-    angulo, math.asin(angulo)))
+print('O angulo {} informado foi O cosseno é O seno é A tangente é '.format(
+    angulo))
