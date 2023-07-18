5699de1c0e5b19dfb552857fc5a50b89e1187c01
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-18 08:44:05 -0300

    alinhando a variavel

---
+++ desafios/desafio005.py
@@ -0,0 +1,5 @@
+nome = input('Qual  é seu nome? ')
+print('Prazer em te conhecer {:=^20}!'.format(nome))
+print('Alinhando o nome na esquerda {:>20}!'.format(nome))
+print('Alinhando o nome na direita {:<20}!'.format(nome))
+print('Alinhando o nome no meio {:^20}!'.format(nome))
