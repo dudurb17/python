d8fc8d8c6bc5497a5089a910254e16cab1679aa1
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-19 19:19:33 -0300

    desafio de descobrir onde está a letra a na frase

---
+++ desafios/desafio027.py
@@ -0,0 +1,6 @@
+frase = str(input('informe uma frase: '))
+
+print('A frase é {}'.format(frase))
+print('Nessa frase tem {} letras a'.format(frase.lower().count('a')))
+print('O primeiro a esta na posiçao {}'.format(frase.lower().find('a')))
+print('O ultimo a está na posição {}'.format(frase.lower().rfind('a')))
