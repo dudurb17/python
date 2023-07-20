31bc7330619e9661136715ed35331eaa1b4758e4
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:42:13 -0300

    Procurando strings dentro de uma variavel

--- desafios/aula09/desafio027.py
+++ desafios/aula09/desafio027.py
@@ -2,5 +2,5 @@ frase = str(input('informe uma frase: '))
 
 print('A frase é {}'.format(frase))
 print('Nessa frase tem {} letras a'.format(frase.lower().count('a')))
-print('O primeiro a esta na posiçao {}'.format(frase.lower().find('a')))
-print('O ultimo a está na posição {}'.format(frase.lower().rfind('a')))
+print('O primeiro a esta na posiçao {}.'.format(frase.lower().find('a')))
+print('O ultimo a está na posição {}.'.format(frase.lower().rfind('a')))
