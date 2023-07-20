24f0ac6b29432a6a193e02c54d1f7e9f81f8b689
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:56:53 -0300

    Sorteando uma ordem na lista

--- desafios/aula08/desafio022.py
+++ desafios/aula08/desafio022.py
@@ -1,8 +1,8 @@
 import random
 aluno1 = str(input('Informe o primeiro aluno: '))
 aluno2 = str(input('Informe o segundo aluno: '))
-aluno3 = str(input('Informe o terceiro nome: '))
-aluno4 = str(input('Informe o quarto nome: '))
+aluno3 = str(input('Informe o terceiro aluno: '))
+aluno4 = str(input('Informe o quarto aluno: '))
 lista = [aluno1, aluno2, aluno3, aluno4]
 random.shuffle(lista)
 print(lista)
