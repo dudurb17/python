7ab528c0a2c7e80206cc2b3728e4e653228eb0e8
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:10:38 -0300

    conversor de real para dolar

--- desafios/aula007/desafio011.py
+++ desafios/aula007/desafio011.py
@@ -1,3 +1,3 @@
-money = float(input('Informe o valor em Reais:'))
+money = float(input('Informe o valor em Reais: '))
 dolar = money/3.27
 print('Com {} reais você pode comprar {} dolar'.format(money, dolar))
