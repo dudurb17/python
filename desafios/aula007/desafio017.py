9a5cdf49ee98096774d2a9f5bb7715fa06741ff5
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:16:42 -0300

    calculando o valor de aluguel de carros

--- desafios/aula007/desafio017.py
+++ desafios/aula007/desafio017.py
@@ -1,4 +1,4 @@
 dia=int(input('Informe quantos dias voc~e utilizou o carro: '))
 km=int(input('Informe quantos Km rodados'))
 valor=(dia*60)+(km*0.15)
-print('O valor sera pago por andar {} dias e andar {} km será de {} '.format(dia,km, valor))
+print('O valor sera pago por andar {} dias e andar {} km será de {}.'.format(dia,km, valor))
