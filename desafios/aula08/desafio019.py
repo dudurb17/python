98e45764cb05d1c6d8d21635cd65974c0494c8cf
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:55:17 -0300

    Calculando a hipotenusa

--- desafios/aula08/desafio019.py
+++ desafios/aula08/desafio019.py
@@ -2,5 +2,5 @@ import math
 
 catetoOp = float(input('Informe o comprimento do cateto oposto: '))
 catetoAdj = float(input('Informe o comprimento do cateto adjacente: '))
-print('O cateto oposto é {}, cateto adjacente {} e a hipotenusa {:.2f}'.format(
+print('O cateto oposto é {}, cateto adjacente {} e a hipotenusa {:.2f}.'.format(
     catetoOp, catetoAdj, math.hypot(catetoOp, catetoAdj)))
