647b9661122f29f51fa67d60cec75186e3987395
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 09:22:47 -0300

    exemplo basico de if e else

---
+++ desafios/Aula010/if/desafio029.py
@@ -0,0 +1,8 @@
+tempo=int(input('Quantos anos tem seu carro? '))
+if tempo<=3:
+    print('Carro novo')
+else:
+    print('carro velho')
+print('===Fim===')
+print('carro novo' if tempo<=3 else 'carro velho')
+print('===Fim===')
