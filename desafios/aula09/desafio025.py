d8779f950159689d062c793075b478f46562f8a3
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:41:21 -0300

    Verificando o primeiro nome da cidade

--- desafios/aula09/desafio025.py
+++ desafios/aula09/desafio025.py
@@ -1,3 +1,3 @@
-cidade = str(input('Informe o nome da sua cidade: '))
+cidade = str(input('Informe o nome da sua cidade:  '))
 print('A tua palavra começa com santos?\n{}'.format(
     cidade.lower().split()[0] in 'santo'))
