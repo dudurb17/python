c3b9ffa3e503c4ee5a9a889143ac31d6959b4a33
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:56:11 -0300

    Sorteando um item na lista

--- desafios/aula08/desafio021.py
+++ desafios/aula08/desafio021.py
@@ -5,4 +5,4 @@ aluno3 = input('Informe o terceiro nome: ')
 aluno4 = input('Informe o quarto nome: ')
 lista = [aluno1, aluno2, aluno3, aluno4]
 
-print('O aluno sorteado para apagar o quadro foi o {}'.format(random.choice(lista)))
+print('O aluno sorteado para apagar o quadro foi o {}.'.format(random.choice(lista)))
