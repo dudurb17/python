e73e4bb78cfe62709e7714278a062b83109ae857
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:09:45 -0300

    mostrando em metros, centimetros e milimetros

--- desafios/aula007/desafio009.py
+++ desafios/aula007/desafio009.py
@@ -1,3 +1,3 @@
 metros = int(input('Informe o valor em metros: '))
-print('Você tem  {} metros, {} centimetros e {} milimetros'.format(
+print('Você tem  {} metros, {} centimetros e {} milimetros.'.format(
     metros, metros*100, metros*1000))
