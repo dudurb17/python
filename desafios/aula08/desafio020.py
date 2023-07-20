dbd51d4ead7c96173c157cfad63e363a0cbc14ee
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:55:46 -0300

    Calculando o cosseno, seno e tang

--- desafios/aula08/desafio020.py
+++ desafios/aula08/desafio020.py
@@ -3,5 +3,5 @@ angulo = int(input('Informe o angulo'))
 cosseno = math.cos(math.radians(angulo))
 seno = math.sin(math.radians(angulo))
 tang = math.tan(math.radians(angulo))
-print('O angulo {} informado foi O cosseno é {:.2f} O seno é {:.2f}A tangente é {:.2f}'.format(
+print('O angulo {} informado foi O cosseno é {:.2f} O seno é {:.2f}A tangente é {:.2f}.'.format(
     angulo, cosseno, seno, tang))
