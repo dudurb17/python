84075397861eb6d401b88594a225efbe0c7a380c
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 08:53:30 -0300

    Verificando se seu nome tem Santos no nome

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -1,5 +1,2 @@
-nome = str(input('informe seu nome: '))
-teste = nome.upper().find('SILVA')
-print('O seu nome tem SILVA? \n{}'.format(teste != -1))
-
-
+nome = str(input('informe seu nome: ')).lower().split()
+print('O seu nome tem Silva?\n{}'.format('silva' in nome))
