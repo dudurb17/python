eea1b0edd87b61e08ad446a3fffb728f3b2ef140
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:15:00 -0300

    Mostrando o dobro, triplo e raiz

--- desafios/aula007/desafio015.py
+++ desafios/aula007/desafio015.py
@@ -1,4 +1,4 @@
 num1 = float(input('Informe um numero:'))
-print('O dobro de {} é {}'.format(num1, num1*2))
-print('O triplo de {} é {}'.format(num1, num1*3))
-print('A raiz quadrada de {} é {:.2f}'.format(num1, num1**(1/2)))
+print('O dobro de {} é {}.'.format(num1, num1*2))
+print('O triplo de {} é {}.'.format(num1, num1*3))
+print('A raiz quadrada de {} é {:.2f}.'.format(num1, num1**(1/2)))
