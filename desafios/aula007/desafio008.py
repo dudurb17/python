552e95356df7884ccd84e1ccd21bdb269558c144
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:08:40 -0300

    calculando a media entre duas notas

--- desafios/aula007/desafio008.py
+++ desafios/aula007/desafio008.py
@@ -1,4 +1,4 @@
 nt1 = float(input('Informe a primeira nota: '))
 nt2 = float(input('Informe a segunda nota: '))
-print('A primeira nota foi {} e a segunda {} e a media é {}'.format(
+print('A primeira nota foi {} e a segunda {} e a media é {}!'.format(
     nt1, nt2, (nt1+nt2)/2))
