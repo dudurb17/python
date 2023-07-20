df4e7f9b019fc080c031a370336361947534baf8
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 11:40:51 -0300

    Separando digitos

--- desafios/aula09/desafio024.py
+++ desafios/aula09/desafio024.py
@@ -3,7 +3,7 @@ u = num1//1 % 10
 d = num1//10 % 10
 c = num1 // 100 % 10
 m = num1//1000 % 10
-print('Unidade: {}'.format(u))
-print('Dezena: {}'.format(d))
-print('Centena: {}'.format(c))
-print('Milhar: {}'.format(m))
+print('Unidade: {}.'.format(u))
+print('Dezena: {}.'.format(d))
+print('Centena: {}.'.format(c))
+print('Milhar: {}.'.format(m))
