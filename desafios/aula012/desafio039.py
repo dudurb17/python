0e30dc9a9235733aaa596bb62bb7f5a2dc7f0c44
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-20 22:45:16 -0300

    numeros sao maiores, menores ou iguais

---
+++ desafios/aula012/desafio039.py
@@ -0,0 +1,4 @@
+num=int(input('Informe o número: '))
+opcao=int('Informe o tipo que vc quer converter:\n1-Binário\n2-octal\n3-hexadecimal')
+if opcao==1:
+    resto==num%2
---
+++ desafios/aula012/desafio040.py
@@ -0,0 +1,9 @@
+num1 = int(input('Informe o numero: '))
+num2 = int(input('Informe o segundo númer: '))
+
+if num1 > num2:
+    print('O primeiro número é maior que o segundo!')
+elif num2 > num1:
+    print('O segundo é maior que o primeiro!')
+else:
+    print('Os numeros são iguais!')
