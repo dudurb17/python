eb9923b795f9b787db218e907ee640c1d5021dc6
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-22 20:08:51 -0300

    for em python

---
+++ desafios/aula013/aula013.py
@@ -0,0 +1,32 @@
+for c in range(0, 7):
+    print(c)
+print('FIM')
+
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+
+
+for c in range(0, 7, 2):
+    print(c)
+print('FIM')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('FIM')
+
+
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
+
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Informe o valor: '))
+    s += n
+print('A soma dos valores é {}!!'.format(s))
