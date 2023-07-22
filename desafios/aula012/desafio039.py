88c78b47cfc769ff8b362b192b2964d9759b692c
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-22 19:27:39 -0300

    base de conversão oct, bin, hex

--- desafios/aula012/desafio039.py
+++ desafios/aula012/desafio039.py
@@ -1,4 +1,11 @@
-num=int(input('Informe o número: '))
-opcao=int('Informe o tipo que vc quer converter:\n1-Binário\n2-octal\n3-hexadecimal')
-if opcao==1:
-    resto==num%2
+num = int(input('Informe o número: '))
+opcao = int(input(
+    'Informe o tipo que vc quer converter:\n1-Binário\n2-octal\n3-hexadecimal\nSua opção:'))
+if opcao == 1:
+    print('O valor digitado é {} e em binario fica {}'.format(num, bin(num)))
+elif opcao == 2:
+    print('O valor digitado é {} e em octal fica {}'.format(num, oct(num)))
+elif opcao == 3:
+    print('O valor digitado é {} e em hexadecimal fica {}'.format(num, hex(num)))
+else:
+    print('Opção invalida')
