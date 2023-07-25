194de83d0fd5a5d93be96eed1d48e1e48d09d3f6
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-25 19:37:42 -0300

    Detector de palíndromo

--- desafios/aula013/desafio055.py
+++ desafios/aula013/desafio055.py
@@ -1,13 +1,25 @@
 
-frase = str(input('Informe a sua frase: ')).lower().strip()
-frase = frase.split()
-fraseC = frase
-frase = ''.join(frase)
-fraseC.reverse()
-fraseC = ''.join(fraseC)
-print(frase)
-print(fraseC)
-if frase == fraseC:
-    print('A frase é palindromo')
+# frase = str(input('Informe a sua frase: ')).lower().strip()
+# frase = frase.split()
+# fraseC = frase
+# frase = ''.join(frase)
+# fraseC.reverse()
+# fraseC = ''.join(fraseC)
+# print(frase)
+# print(fraseC)
+# if frase == fraseC:
+#     print('A frase é palindromo')
+# else:
+#     print('A frase não é palindromo')
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+# for letra in range(len(junto)-1, -1, -1):
+#     inverso += junto[letra]
+
+if inverso == junto:
+    print('Temos um palíndromo')
 else:
-    print('A frase não é palindromo')
+    print('A frase digitada não é um palíndromo')
