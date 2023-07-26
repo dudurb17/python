0352a605a0b74276d916811b0313a845c359de5f
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-26 15:47:56 -0300

    repetição while

---
+++ desafios/aula014/desafio061.py
@@ -0,0 +1,13 @@
+
+finalizar="N"
+num1=int(input("Informe o primeiro número: "))
+num2=int(input("Informe o segundo número: "))
+while finalizar=="N":
+    opcao=int(input(''' Informe o que você deseja fazer com o numero
+    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] Novos números
+    [5] sair do Programa
+    Sua opção:''' ))
+    
