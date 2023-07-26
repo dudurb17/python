bdeb8cc61c2e59662f16af1c2cc66151a21c1e4d
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-26 16:38:28 -0300

    criando menu de opções

--- desafios/aula014/desafio061.py
+++ desafios/aula014/desafio061.py
@@ -1,8 +1,10 @@
 
 finalizar="N"
-num1=int(input("Informe o primeiro número: "))
-num2=int(input("Informe o segundo número: "))
+opcao=4
 while finalizar=="N":
+    if opcao==4:
+        num1=int(input("Informe o primeiro número: "))
+        num2=int(input("Informe o segundo número: "))
     opcao=int(input(''' Informe o que você deseja fazer com o numero
     [1] Somar
     [2] Multiplicar
@@ -10,4 +12,16 @@ while finalizar=="N":
     [4] Novos números
     [5] sair do Programa
     Sua opção:''' ))
-    
+    if opcao==1:
+        print("A soma do numero {} e do número {} é de {}".format(num1,num2, num1+num2))
+    elif opcao==2:
+        print("A multiplicação do numero {} e do número {} é de {}".format(num1, num2, num1*num2))
+    elif opcao==3:
+        if num1>num2:
+            print("O maior número é o {}".format(num1))
+        else:
+             print("O maior número é o {}".format(num2))
+    elif opcao==5:
+        finalizar="S"
+    else:
+        print("Opção invalida")
