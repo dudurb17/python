c129bb02ea1185055d7b94b315b22b89f4fac01e
dudurb17 <eduardo.robettibedin@gmail.com>
2023-07-30 17:59:38 -0300

    aula015

---
+++ desafios/aula015/aula015.py
@@ -0,0 +1,23 @@
+cont = 1
+while cont <= 10:
+    print(cont, "->", end='')
+    cont += 1
+print('Acabou')
+
+n = cont = 0
+while cont < 3:
+    n = int(input('Digite um número: '))
+    cont += 1
+
+
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')
+
+nome = 'Eduardo'
+idade = 18
+print(f'O {nome} tem {idade} anos.')
